0c0a30da18a159a740533dbb4a0916b2cc366c47
IvanaZandona <maribeliva2.0@gmail.com>
2023-11-23 05:41:53 -0300

    mas cambios

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,16 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import TaskForm from "./components/TaskForm/TaskForm";
-import TaskList from './components/TaskList/TaskList';
 
 function App() {
 
   return (
     <BrowserRouter>
     
-    <NavBar />
     <Routes>
-      {/* <Route path="/tasklist" element={<TaskList />} /> */}
+      <Route path="/" element={<NavBar />} />
       <Route path="/taskform" element={<TaskForm />} />
     </Routes>
-    
-    {/* <TaskForm /> */}
 
     </BrowserRouter>
   )
